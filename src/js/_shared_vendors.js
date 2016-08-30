847f4c25b35eba4f2a96faea6d6ca940bf02dd40
Andy Ruck <mychiara@gmail.com>
2016-08-30 18:15:40 +0200

    added phaser to the mix
    - more stylings
    - added tabs to settings details

--- src/js/_shared_vendors.js
+++ src/js/_shared_vendors.js
@@ -5,7 +5,13 @@
 
 'use strict';
 
+// angular specific
 require('angular');
 require('angular-ui-router');
 require('angular-resource');
 require('angular-ui-bootstrap');
+
+// others
+window.PIXI = require('phaser/build/pixi');
+window.p2 = require('phaser/build/p2')
+require('phaser/build/phaser');
