d651860642f65228e3f65215c4f65006be7c73e0
Andy Ruck <mychiara@gmail.com>
2016-09-01 17:03:36 +0200

    db connection test 4

--- conf/config.staging.js
+++ conf/config.staging.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   couchbase: {
-    endpoint: "http://couchbase:8091",
+    endpoint: "172.18.0.2:8091",
     n1ql: "hns_couchbase:8093",
     bucket: "hide_n_seek",
     detailed_errors: 1,
