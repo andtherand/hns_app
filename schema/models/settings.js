4a634ec56797419f8608c8543d2a2b7ead702cdc
Andy Ruck <mychiara@gmail.com>
2016-09-01 11:10:19 +0200

    removed the save and create function in the settings model due to the fact that we're using the modelinstance method

--- schema/models/settings.js
+++ schema/models/settings.js
@@ -46,26 +46,6 @@ var SettingsModel = ottoman.model('Settings', {
 );
 
 // ---------------------------------------------
-// - FUNCTIONS
-
-/**
- * saves and creates a new instance of the settings object
- * also passes a callback to check for success
- *
- * @param grid      sets the grid params
- * @param teamRed   sets the features of team red
- * @param teamBlue  sets the features of team blue
- * @param done      callback function
- */
-SettingsModel.createAndSave = function createAndSave(grid, teamRed, teamBlue, done) {
-  this.create({
-      grid: grid,
-      teamRed: teamRed,
-      teamBlue: teamBlue
-  }, done);
-};
-
-// ---------------------------------------------
 // - MODULE
 
 module.exports = SettingsModel;
